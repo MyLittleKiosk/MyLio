eab015b52cb21f3131bf19f6c12f106c23f4d3df
singwisdom <57509973+singwisdom@users.noreply.github.com>
2025-04-30 01:25:35 +0900

    Feat: 옵션 상세삭제 Service 로직 구현
    S12P31B102-104

--- BE/src/main/java/com/ssafy/mylio/domain/options/service/OptionDetailService.java
+++ BE/src/main/java/com/ssafy/mylio/domain/options/service/OptionDetailService.java
@@ -1,13 +1,38 @@
 package com.ssafy.mylio.domain.options.service;
 
+import com.ssafy.mylio.domain.options.entity.OptionDetail;
+import com.ssafy.mylio.domain.options.repository.OptionDetailRepository;
+import com.ssafy.mylio.domain.store.entity.Store;
+import com.ssafy.mylio.domain.store.repository.StoreRepository;
+import com.ssafy.mylio.global.error.code.ErrorCode;
+import com.ssafy.mylio.global.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class OptionDetailService {
 
-    public void deleteOptionDetail(Integer storeId, Integer optionDetailId){
+    private final OptionDetailRepository optionDetailRepository;
+    private final StoreRepository storeRepository;
 
+    @Transactional
+    public void deleteOptionDetail(Integer storeId, Integer optionDetailId){
+        Store store = getStoreId(storeId);
+        OptionDetail optionDetail = optionDetailRepository.findById(optionDetailId)
+                .orElseThrow(()-> new CustomException(ErrorCode.OPTION_DETAIL_NOT_FOUND, "optionDetailId", optionDetailId));
+
+        if (!optionDetail.getOptions().getStore().getId().equals(store.getId())) {
+            throw new CustomException(ErrorCode.OPTION_STORE_NOT_MATCH, "optionDetailId", optionDetailId);
+        }
+
+        optionDetail.delete();
+    }
+
+    private Store getStoreId(Integer storeId){
+        return storeRepository.findById(storeId)
+                .orElseThrow(()-> new CustomException(ErrorCode.STORE_NOT_FOUND, "storeId", storeId));
     }
 }
