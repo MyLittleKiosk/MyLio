f598fd62a191d3a7d14707da022a1970ba28ab92
singwisdom <57509973+singwisdom@users.noreply.github.com>
2025-04-30 01:11:23 +0900

    Feat: 옵션 상세삭제 Cotroller 구현
    S12P31B102-104

---
+++ BE/src/main/java/com/ssafy/mylio/domain/options/service/OptionDetailService.java
@@ -0,0 +1,13 @@
+package com.ssafy.mylio.domain.options.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OptionDetailService {
+
+    public void deleteOptionDetail(Integer storeId, Integer optionDetailId){
+
+    }
+}
