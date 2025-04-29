d3bc92252d9fc05e1730b90558cb08fe40eee3a9
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-04-29 15:39:48 +0900

    Rename: MSWInit 위치 변경
    S12P31B102-59

--- FE/kiosk/src/App.tsx
+++ FE/kiosk/src/App.tsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import './App.css';
 import Login from '@/pages/Login';
+import MSWInit from './components/common/MSWInit';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/kiosk' element={<Outlet />}>
-          <Route index element={<div>홈페이지</div>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <MSWInit />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/kiosk' element={<Outlet />}>
+            <Route index element={<div>홈페이지</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
