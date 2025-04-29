1a78ca952fab07171749d808a7b7a9a1f3a98843
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-04-29 14:15:37 +0900

    Feat: 라우팅 설정 추가

--- FE/kiosk/src/App.tsx
+++ FE/kiosk/src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <>시작</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Outlet />}>
+          <Route index element={<div>홈페이지</div>} />
+          <Route path='login' element={<div>로그인 페이지</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
