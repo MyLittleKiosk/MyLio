233e570b37bff9113f0ca890dfa8feba36042497
HANoeJ <63058756+HANoeJ@users.noreply.github.com>
2025-04-29 17:04:15 +0900

    Chore: 역할 description 수정
    S12P31B102-85

--- BE/src/main/java/com/ssafy/mylio/domain/account/entity/AccountRole.java
+++ BE/src/main/java/com/ssafy/mylio/domain/account/entity/AccountRole.java
@@ -2,8 +2,8 @@ package com.ssafy.mylio.domain.account.entity;
 
 public enum AccountRole {
     STORE("STORE","매장"),
-    SUPER("SUPER","매장"),
-    KIOSK("KIOSK","매장");
+    SUPER("SUPER","관리자"),
+    KIOSK("KIOSK","키오스크");
 
     private final String code;
     private final String description;
