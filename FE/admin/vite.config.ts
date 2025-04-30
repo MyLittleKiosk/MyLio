5f20ec37d40f2e9342ccfa40fa0f04f38dcfc9e9
이하영 <lhy23456@gmail.com>
2025-04-30 17:18:59 +0900

    Chore: https 설정

--- FE/admin/vite.config.ts
+++ FE/admin/vite.config.ts
@@ -1,8 +1,16 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import fs from 'fs';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    },
+    port: 3000,
+  },
 });
