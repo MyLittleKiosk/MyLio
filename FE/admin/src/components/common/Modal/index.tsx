3d3b02a46b4a81c03ea1b99f14e22ad8e055299f
이하영 <lhy23456@gmail.com>
2025-04-30 14:24:20 +0900

    Feat: 공통 모달 구현 - Modal 상태 관리 전역 store 생성 - 모달 사용하고 싶은 컴포넌트에 <Modal /> 컴포넌트 추가 - openModal에 원하는 모달 컴포넌트 전달 S12P31B102-123

---
+++ FE/admin/src/components/common/Modal/index.tsx
@@ -0,0 +1,74 @@
+import React, { useRef, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+import useModalStore from '@/stores/useModalStore';
+
+const Modal = () => {
+  const { isOpen, isClosing, modalContent, closeModal } = useModalStore();
+  const dialog = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (isClosing) return;
+
+    if (isOpen && dialog.current) {
+      dialog.current.showModal();
+    }
+  }, [isOpen, isClosing]);
+
+  function handleBackdropClick(
+    event: React.MouseEvent<HTMLDialogElement, MouseEvent>
+  ) {
+    if (dialog.current && event.target === dialog.current) {
+      closeModal();
+    }
+  }
+
+  const modalVariants = {
+    start: {
+      opacity: 0,
+      y: 150,
+    },
+    end: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.3,
+        ease: 'easeOut',
+      },
+    },
+    exit: {
+      opacity: 0,
+      y: 150,
+      transition: {
+        duration: 0.3,
+        ease: 'easeIn',
+      },
+    },
+  };
+
+  return (
+    <AnimatePresence
+      onExitComplete={() => {
+        if (dialog.current) {
+          dialog.current.close();
+        }
+      }}
+    >
+      {isOpen && (
+        <motion.dialog
+          initial='start'
+          animate='end'
+          exit='exit'
+          variants={modalVariants}
+          ref={dialog}
+          onClick={handleBackdropClick}
+          className={`w-[50%] min-w-[200px] max-w-[600px] fixed top-0 z-50 mx-auto rounded-lg`}
+        >
+          {modalContent}
+        </motion.dialog>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Modal;
--- FE/admin/src/pages/statistics/index.tsx
+++ FE/admin/src/pages/statistics/index.tsx
@@ -1,5 +1,16 @@
+import Modal from '@/components/common/Modal';
+import useModalStore from '@/stores/useModalStore';
+
 const Statistics = () => {
-  return <div>Statistics</div>;
+  const { openModal } = useModalStore();
+  return (
+    <div className='w-full'>
+      <button onClick={() => openModal(<div className='w-12 h-10'>Modal</div>)}>
+        Open Modal
+      </button>
+      <Modal />
+    </div>
+  );
 };
 
 export default Statistics;
