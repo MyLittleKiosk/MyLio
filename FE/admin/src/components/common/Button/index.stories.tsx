9cc0688fee5b2d465b4de96a8a9e297124b9eaf7
이하영 <lhy23456@gmail.com>
2025-04-30 17:41:05 +0900

    Fix: story 내 변수명 오류 해결

--- FE/admin/src/components/common/Button/index.stories.tsx
+++ FE/admin/src/components/common/Button/index.stories.tsx
@@ -14,7 +14,7 @@ const meta = {
     icon: { control: 'object', table: { disable: true } },
     onClick: { action: 'clicked', table: { disable: true } },
     className: { control: 'text', table: { disable: true } },
-    type: { control: 'select', table: { disable: true } },
+    buttonType: { control: 'select', table: { disable: true } },
   },
 } satisfies Meta<typeof Button>;
 
@@ -25,7 +25,7 @@ type Story = StoryObj<typeof meta>;
 //텍스트만 있는 버튼
 export const Default: Story = {
   args: {
-    type: 'button',
+    buttonType: 'button',
     text: 'Button',
   },
 };
