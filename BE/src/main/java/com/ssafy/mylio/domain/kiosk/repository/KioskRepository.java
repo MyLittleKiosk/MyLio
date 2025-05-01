d9d4c5062249b76d241711cbb8fdfef7d2a77096
HANoeJ <63058756+HANoeJ@users.noreply.github.com>
2025-05-02 00:36:26 +0900

    Feat: 키오스크 목록 조회
    S12P31B102-144

--- BE/src/main/java/com/ssafy/mylio/domain/kiosk/repository/KioskRepository.java
+++ BE/src/main/java/com/ssafy/mylio/domain/kiosk/repository/KioskRepository.java
@@ -1,9 +1,11 @@
 package com.ssafy.mylio.domain.kiosk.repository;
 
 import com.ssafy.mylio.domain.kiosk.entity.KioskSession;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 
 @Repository
@@ -11,5 +13,11 @@ public interface KioskRepository extends JpaRepository<KioskSession,Integer> {
     Optional<KioskSession> findByStoreIdAndId(Integer storeId, Integer id);
 
     Optional<KioskSession> findByStoreIdAndName(Integer storeId, String name);
+
+    //키워드 없을 때 검색
+    Page<KioskSession> findByStoreId(Integer storeId, Pageable pageable);
+
+    //키워드 있는 경우
+    Page<KioskSession> findByStoreIdAndNameContainingIgnoreCase(Integer storeId,String keywork,Pageable pageable);
 }
 
