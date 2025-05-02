0a2d4dcc73cd281dd9d9abfae992e3196f1ebcc1
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-05-02 09:59:54 +0900

    Feat: 키오스크 아이디 인풋 추가

--- FE/kiosk/src/pages/Login/index.tsx
+++ FE/kiosk/src/pages/Login/index.tsx
@@ -3,8 +3,8 @@ import Button from '@/components/common/Button';
 const Login = () => {
   return (
     <>
-      <div className='flex flex-col items-center justify-center h-screen bg-primary/10 gap-3'>
-        <div className='flex flex-col gap-3 max-w-md'>
+      <div className='flex flex-col items-center justify-center h-screen bg-primary gap-3'>
+        <section className='flex flex-col gap-3 max-w-md bg-white p-10 rounded-md'>
           <h1 className='text-7xl font-bold text-primary text-center'>MaLio</h1>
           <div className='flex flex-col'>
             <label htmlFor='id' className='text-sm text-gray-500'>
@@ -25,8 +25,17 @@ const Login = () => {
               type='password'
             />
           </div>
+          <div>
+            <label htmlFor='kiosk' className='text-sm text-gray-500'>
+              키오스크 아이디
+            </label>
+            <input
+              className='w-full rounded-md border border-gray-300 px-3 py-3 max-w-sm'
+              type='text'
+            />
+          </div>
           <Button content='로그인' onClick={() => {}} />
-        </div>
+        </section>
       </div>
     </>
   );
