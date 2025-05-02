214401e82570452573b8317c6a4b57cd49e9ec2a
singwisdom <57509973+singwisdom@users.noreply.github.com>
2025-05-02 13:54:14 +0900

    Refactor: 결제 Status 변경
    S12P31B102-147

--- BE/src/main/java/com/ssafy/mylio/domain/sales/entity/YearlyPaymentMethodRatio.java
+++ BE/src/main/java/com/ssafy/mylio/domain/sales/entity/YearlyPaymentMethodRatio.java
@@ -28,7 +28,7 @@ public class YearlyPaymentMethodRatio extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "method", nullable = false)
-    private PaymentMethod method;
+    private PaymentStatus method;
 
     @Column(name = "ratio", nullable = false, precision = 5, scale = 2)
     private BigDecimal ratio;
