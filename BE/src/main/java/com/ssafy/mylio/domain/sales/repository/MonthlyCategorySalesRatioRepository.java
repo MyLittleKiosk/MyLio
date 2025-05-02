308e12041ecdcdd7ebdd050907ab2d663a81c408
singwisdom <57509973+singwisdom@users.noreply.github.com>
2025-05-02 13:45:32 +0900

    Feat: Repository에 통계조회 함수 추가
    S12P31B102-147

--- BE/src/main/java/com/ssafy/mylio/domain/sales/repository/MonthlyCategorySalesRatioRepository.java
+++ BE/src/main/java/com/ssafy/mylio/domain/sales/repository/MonthlyCategorySalesRatioRepository.java
@@ -5,9 +5,14 @@ import com.ssafy.mylio.domain.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MonthlyCategorySalesRatioRepository  extends JpaRepository<MonthlyCategorySalesRatio, Integer> {
 
     // 해당 매장·연도·월의 모든 통계 레코드 삭제
     void deleteByStoreAndYearAndMonth(Store store, int year, int month);
+
+    // 월별 통계 조회
+    List<MonthlyCategorySalesRatio> findByStoreIdAndYearAndMonth(Integer storeId, int year, int month);
 }
