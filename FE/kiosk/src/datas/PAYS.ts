8dbec62c31c6c7b7de525b923a7688fb902456a1
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-05-02 15:22:12 +0900

    Feat: 결제 수단 선택 퍼블리싱

---
+++ FE/kiosk/src/datas/PAYS.ts
@@ -0,0 +1,18 @@
+export const PAY_METHODS = [
+  {
+    id: 1,
+    name: '현금',
+  },
+  {
+    id: 2,
+    name: '신용카드',
+  },
+  {
+    id: 3,
+    name: '기프트카드',
+  },
+  {
+    id: 4,
+    name: '카카오페이',
+  },
+];
