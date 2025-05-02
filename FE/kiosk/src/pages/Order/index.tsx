64760e91909f44d084e86dd7143aded497e3079e
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-05-02 13:07:01 +0900

    Style: 메뉴 화면 margin 수정

--- FE/kiosk/src/pages/Order/index.tsx
+++ FE/kiosk/src/pages/Order/index.tsx
@@ -2,19 +2,19 @@ import { MenuResponse } from '@/service/mock/dummies/Order';
 
 const Order = () => {
   return (
-    <section className='flex flex-col w-full h-full justify-center items-center p-10'>
-      <h1 className='text-2xl font-bold mb-10'>주문하기</h1>
-      <div className='w-full items-start mb-2'>
-        <span className='font-bold'>
+    <section className='flex flex-col w-full h-full px-10 pt-10'>
+      <h1 className='text-2xl font-bold inline-block'>메뉴</h1>
+      <div className='w-full items-start mb-4'>
+        <span className='font-bold text-gray-500 text-sm'>
           총 {MenuResponse.menu.length}개의 메뉴
         </span>
       </div>
-      <div className='grid grid-cols-4 justify-items-start gap-5 overflow-y-auto h-[75%] w-full'>
+      <div className='grid grid-cols-4 justify-items-start gap-5 overflow-y-auto'>
         {MenuResponse.menu.map((item) => {
           return (
             <div
               key={item.id}
-              className='flex flex-col items-center justify-center w-[180px] h-[180px] text-center break-keep border-2 border-gray-300 rounded-3xl'
+              className='flex flex-col items-center justify-center w-[180px] h-[180px] text-center break-keep rounded-3xl'
             >
               <img
                 src={item.image}
