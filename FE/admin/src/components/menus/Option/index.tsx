1c1732843f42230dfd618c813611388d09d24947
이하영 <lhy23456@gmail.com>
2025-05-04 01:48:35 +0900

    Feat: Option 추가 모달 퍼블리싱 - 추후 리팩토링 시, 인풋 리팩토링 예정 - 영문명은 자동번역 추가 예정

--- FE/admin/src/components/menus/Option/index.tsx
+++ FE/admin/src/components/menus/Option/index.tsx
@@ -1,10 +1,19 @@
+import IconAdd from '@/assets/icons/IconAdd';
+
+import Button from '@/components/common/Button';
 import Table from '@/components/common/Table';
+import AddOptionGroupModal from '@/components/menus/AddOptionGroupModal';
+
 import { CategoryList } from '@/types/categories';
 import { MenuList, NavItemType } from '@/types/menus';
 import { OptionList, OptionType } from '@/types/options';
 import { Column } from '@/types/tableProps';
 
+import useModalStore from '@/stores/useModalStore';
+
 const Option = ({ selectedNav }: { selectedNav: NavItemType }) => {
+  const { openModal } = useModalStore();
+
   const isOptionData = (
     data: MenuList | CategoryList | OptionList
   ): data is OptionList => {
@@ -12,7 +21,19 @@ const Option = ({ selectedNav }: { selectedNav: NavItemType }) => {
   };
 
   return (
-    <>
+    <div className='flex flex-col gap-2'>
+      <div className='flex gap-2 max-h-[10%] w-full items-center justify-between'>
+        <h3 className='text-lg font-preMedium'>옵션 관리</h3>
+        <Button
+          buttonType='button'
+          text='옵션그룹 추가'
+          icon={<IconAdd fillColor='white' />}
+          onClick={() => {
+            openModal(<AddOptionGroupModal />);
+          }}
+          className='items-center justify-center'
+        />
+      </div>
       <Table<OptionType>
         title='옵션 목록'
         description={`총 6개의 옵션이 있습니다.`}
@@ -21,7 +42,7 @@ const Option = ({ selectedNav }: { selectedNav: NavItemType }) => {
           isOptionData(selectedNav.data) ? selectedNav.data.content.options : []
         }
       />
-    </>
+    </div>
   );
 };
 
