817e17fd8ae96ab8953105d96d90bf29137f44a3
이해루 <gofn080776@gmail.com>
2025-05-05 00:19:34 +0900

    Feat: 키오스크 추가 모달 구현

--- FE/admin/src/pages/Kiosk/index.tsx
+++ FE/admin/src/pages/Kiosk/index.tsx
@@ -7,6 +7,7 @@ import { KIOSK_COLUMNS, KIOSK_LIST } from '@/datas/kioskList';
 import Modal from '@/components/common/Modal';
 import useModalStore from '@/stores/useModalStore';
 import React, { useState } from 'react';
+import AddKioskModal from '@/components/kiosks/AddKioskModal';
 
 const Kiosk = () => {
   const { openModal } = useModalStore();
@@ -24,29 +25,31 @@ const Kiosk = () => {
     <section className='w-full h-full p-4 flex flex-col gap-2 '>
       <h1 className='text-2xl font-preBold h-[5%]'>키오스크 목록</h1>
       <div className='flex gap-2 max-h-[10%] w-full justify-between'>
-        <Input
-          inputId='searchKiosk'
-          placeholder='키오스크명 또는 위치로 검색'
-          inputType='text'
-          inputValue={searchValue}
-          onChange={handleSearchChange}
-          className='w-[65%]'
-        />
-        <Select
-          options={['a', 'b', 'c']}
-          selected={selected}
-          onChange={handleSelectChange}
-          placeholder='모든 키오스크'
-          className='w-[11%]'
-          getOptionLabel={(option) => option}
-          getOptionValue={(option) => option}
-        />
+        <div className='flex gap-2 w-full'>
+          <Input
+            inputId='searchKiosk'
+            placeholder='키오스크명 또는 위치로 검색'
+            inputType='text'
+            inputValue={searchValue}
+            onChange={handleSearchChange}
+            className='w-[65%]'
+          />
+          <Select
+            options={['a', 'b', 'c']}
+            selected={selected}
+            onChange={handleSelectChange}
+            placeholder='전체'
+            className='w-[11%] h-full'
+            getOptionLabel={(option) => option}
+            getOptionValue={(option) => option}
+          />
+        </div>
         <Button
           buttonType='button'
           text='키오스크 추가'
           icon={<IconAdd fillColor='white' />}
           onClick={() => {
-            openModal(<div>open</div>);
+            openModal(<AddKioskModal />);
           }}
           className='w-[11%] items-center justify-center'
         />
