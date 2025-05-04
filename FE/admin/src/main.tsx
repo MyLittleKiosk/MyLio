53fbaa01fc1ce7d222afad3e2a82cfbfda905c14
이병조 <qudwh0312@naver.com>
2025-05-04 17:40:17 +0900

    Fix : base 출력 코드 추가

--- FE/admin/src/main.tsx
+++ FE/admin/src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 
 const basename = import.meta.env.BASE_URL
+console.log('🚀 BASE_URL =', basename);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
