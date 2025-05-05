7714a344670d17217ab32aac014f718b9df3665d
이하영 <lhy23456@gmail.com>
2025-05-05 11:46:36 +0900

    Fix: 카테고리 추가 모달 수정

--- FE/admin/src/components/menus/AddCategoryModal/index.tsx
+++ FE/admin/src/components/menus/AddCategoryModal/index.tsx
@@ -9,7 +9,7 @@ import useModalStore from '@/stores/useModalStore';
 const AddCategoryModal = () => {
   const { closeModal } = useModalStore();
 
-  const [tagValue, setTagValue] = useState('');
+  const [categoryValue, setCategoryValue] = useState('');
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -19,19 +19,19 @@ const AddCategoryModal = () => {
   return (
     <div className='flex flex-col gap-8 px-10 py-8'>
       <ModalHeader
-        title='새 태그 추가'
-        description='새로운 태그 이름을 입력하세요. '
+        title='새 카테고리 추가'
+        description='새로운 카테고리를 입력하세요. '
       />
 
       <form onSubmit={handleSubmit}>
         <div className='w-full flex flex-col gap-2 '>
           <Input
-            inputId='tagAdd'
-            label='태그명'
+            inputId='categoryAdd'
+            label='카테고리명'
             inputType='text'
-            onChange={(e) => setTagValue(e.target.value)}
-            inputValue={tagValue}
-            placeholder='태그명을 입력하세요.'
+            onChange={(e) => setCategoryValue(e.target.value)}
+            inputValue={categoryValue}
+            placeholder='카테고리명을 입력하세요.'
           />
           <div className='flex justify-end gap-2'>
             <Button
