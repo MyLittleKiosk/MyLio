87b6a14f247e128f1f5afaa3a8f90fdfb843e5ef
강성엽 <piz115@naver.com>
2025-05-07 16:26:38 +0900

    Feat: 기간별 매출 그래프 구현
    - chart.js와 react-chartjs-2를 활용해 선 그래프 구현
    - 연월 선택으로 그래프 데이터 셋 구성
    - 빈 날짜의 데이터는 값을 0으로 추가하여 그래프 구성
    
    S12P31B102-160

--- FE/admin/src/App.tsx
+++ FE/admin/src/App.tsx
@@ -3,16 +3,20 @@ import { Route, Routes } from 'react-router-dom';
 import MSWInit from '@/components/common/MSWInit';
 import Layout from '@/components/layout';
 
+import Accounts from '@/pages/Accounts';
+import Kiosk from '@/pages/Kiosk';
 import Login from '@/pages/Login';
 import Menus from '@/pages/Menus';
-import Kiosk from '@/pages/Kiosk';
-import Statistics from '@/pages/Statistics';
-import Accounts from '@/pages/Accounts';
 import Orders from '@/pages/Orders';
+import Statistics from '@/pages/Statistics';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
 function App() {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Routes>
         <Route element={<Layout />}>
           <Route path='/' element={<Statistics />} />
@@ -24,7 +28,7 @@ function App() {
         <Route path='/login' element={<Login />} />
       </Routes>
       <MSWInit />
-    </>
+    </QueryClientProvider>
   );
 }
 
