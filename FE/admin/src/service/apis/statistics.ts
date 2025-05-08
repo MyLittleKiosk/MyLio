99eeb609b71ef4108418aabc13ae981a3ec9adfc
강성엽 <piz115@naver.com>
2025-05-08 12:09:51 +0900

    Feat: 파이 차트 API 연결

--- FE/admin/src/service/apis/statistics.ts
+++ FE/admin/src/service/apis/statistics.ts
@@ -1,6 +1,12 @@
-import authClient from '@/service/client';
+import authClient from '@/service/authClient';
 import { Response } from '@/types/apiResponse';
-import SalesTrendType from '@/types/statistics';
+import {
+  CategorySalesRatioType,
+  DailySalesStatisticsType,
+  OrderSalesRatioType,
+  PaymentSalesRatioType,
+  SalesTrendType,
+} from '@/types/statistics';
 
 export async function getSalesTrend(
   year: number,
@@ -11,6 +17,66 @@ export async function getSalesTrend(
     const res = await authClient.get('/sales', {
       params,
     });
+    return res.data;
+  } catch (e: unknown) {
+    if (e instanceof Error) throw new Error(e.message);
+    else throw new Error('unknown Error');
+  }
+}
+
+export async function getStatisticsByCategory(
+  year: number,
+  month?: number
+): Promise<Response<CategorySalesRatioType[]>> {
+  try {
+    const params = month ? { year, month } : { year };
+    const res = await authClient.get('/sales/by_category', {
+      params,
+    });
+    return res.data;
+  } catch (e: unknown) {
+    if (e instanceof Error) throw new Error(e.message);
+    else throw new Error('unknown Error');
+  }
+}
+
+export async function getStatisticsByOrder(
+  year: number,
+  month?: number
+): Promise<Response<OrderSalesRatioType[]>> {
+  try {
+    const params = month ? { year, month } : { year };
+    const res = await authClient.get('/sales/by_order', {
+      params,
+    });
+    return res.data;
+  } catch (e: unknown) {
+    if (e instanceof Error) throw new Error(e.message);
+    else throw new Error('unknown Error');
+  }
+}
+
+export async function getStatisticsByPayment(
+  year: number,
+  month?: number
+): Promise<Response<PaymentSalesRatioType[]>> {
+  try {
+    const params = month ? { year, month } : { year };
+    const res = await authClient.get('/sales/by_payment', {
+      params,
+    });
+    return res.data;
+  } catch (e: unknown) {
+    if (e instanceof Error) throw new Error(e.message);
+    else throw new Error('unknown Error');
+  }
+}
+
+export async function getStatisticsByDaily(): Promise<
+  Response<DailySalesStatisticsType[]>
+> {
+  try {
+    const res = await authClient.get('/sales/by_daily');
     return res.data;
   } catch (e: unknown) {
     if (e instanceof Error) throw new Error(e.message);
--- FE/admin/src/types/statistics.ts
+++ FE/admin/src/types/statistics.ts
@@ -3,4 +3,30 @@ type SalesTrendType = {
   sales: number;
 };
 
-export default SalesTrendType;
+type DailySalesStatisticsType = {
+  totalSales: number;
+  totalOrders: number;
+};
+
+type PaymentSalesRatioType = {
+  paymentName: string;
+  ratio: number;
+};
+
+type OrderSalesRatioType = {
+  orderTypeName: string;
+  ratio: number;
+};
+
+type CategorySalesRatioType = {
+  categoryName: string;
+  ratio: number;
+};
+
+export type {
+  CategorySalesRatioType,
+  DailySalesStatisticsType,
+  OrderSalesRatioType,
+  PaymentSalesRatioType,
+  SalesTrendType,
+};
