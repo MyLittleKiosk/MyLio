eb7ef0a690f92a3afdac4541ff80a6040bf0d805
강성엽 <piz115@naver.com>
2025-05-08 17:14:30 +0900

    Feat: 계정 관리 API 추가
    - react query를 활용해 데이터 처리&관리
    
    S12P31B102-166

---
+++ FE/admin/src/service/queries/account.ts
@@ -0,0 +1,117 @@
+import { AccountForm } from '@/types/account';
+import {
+  deleteAccount,
+  getAccountDetail,
+  getAccountList,
+  patchAccount,
+  patchAccountPassword,
+  patchResetPassword,
+  postAccount,
+} from '@services/apis/account';
+import { QueryClient, useMutation, useQuery } from '@tanstack/react-query';
+
+export const useGetAccountList = () => {
+  const query = useQuery({
+    queryKey: ['accountList'],
+    queryFn: () => getAccountList(),
+  });
+
+  return {
+    data: query.data?.data,
+    isLoading: query.isLoading,
+    isError: query.isError,
+  };
+};
+
+export const usePostAccount = (account: AccountForm) => {
+  const queryClient = new QueryClient();
+  const query = useMutation({
+    mutationFn: () => postAccount(account),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['accountList'] });
+    },
+  });
+
+  return {
+    mutate: query.mutate,
+    isPending: query.isPending,
+    isError: query.isError,
+  };
+};
+
+export const useDeleteAccount = (accountId: number) => {
+  const queryClient = new QueryClient();
+  const query = useMutation({
+    mutationFn: () => deleteAccount(accountId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['accountList'] });
+    },
+  });
+
+  return {
+    mutate: query.mutate,
+    isPending: query.isPending,
+    isError: query.isError,
+  };
+};
+
+export const useGetAccountDetail = (accountId: number) => {
+  const query = useQuery({
+    queryKey: ['accountDetail', accountId],
+    queryFn: () => getAccountDetail(accountId),
+  });
+
+  return {
+    data: query.data?.data,
+    isLoading: query.isLoading,
+    isError: query.isError,
+  };
+};
+
+export const usePatchAccount = (accountId: number, account: AccountForm) => {
+  const queryClient = new QueryClient();
+  const query = useMutation({
+    mutationFn: () => patchAccount(accountId, account),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['accountList'] });
+    },
+  });
+
+  return {
+    mutate: query.mutate,
+    isPending: query.isPending,
+    isError: query.isError,
+  };
+};
+
+export const usePatchAccountPassword = (nowPw: string, newPw: string) => {
+  const queryClient = new QueryClient();
+  const query = useMutation({
+    mutationFn: () => patchAccountPassword(nowPw, newPw),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['accountList'] });
+    },
+  });
+
+  return {
+    mutate: query.mutate,
+    isPending: query.isPending,
+    isError: query.isError,
+  };
+};
+
+export const usePatchResetPassword = (email: string, username: string) => {
+  const queryClient = new QueryClient();
+  const query = useMutation({
+    mutationFn: () => patchResetPassword(email, username),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['accountList'] });
+    },
+  });
+
+  return {
+    mutate: query.mutate,
+    isPending: query.isPending,
+    isError: query.isError,
+  };
+};
