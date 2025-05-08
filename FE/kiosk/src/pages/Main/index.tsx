7765fb1b3b599501c7daa63f058b399b3b3a2834
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-05-08 16:22:01 +0900

    Feat: STT버튼 컴포넌트 구현

--- FE/kiosk/src/pages/Main/index.tsx
+++ FE/kiosk/src/pages/Main/index.tsx
@@ -1,23 +1,23 @@
-import ChatContainer from '@/components/Chat/ChatContainer';
 import { useLocation } from 'react-router-dom';
+import ChatContainer from '@/components/Chat/ChatContainer';
+import { useAudioRecord } from '@/hooks/useAudioRecord';
 
 interface Props {
   userChat: string;
   gptChat: string;
-  isRecording: boolean;
-  volume: number;
 }
 /**
  * Clova 테스트 페이지
  */
-const Main = ({ isRecording, volume }: Props) => {
+const Main = ({ userChat, gptChat }: Props) => {
   const location = useLocation();
+  const { isRecording, volume } = useAudioRecord();
 
   return (
     <div className='flex flex-col items-center justify-center p-4 '>
       <ChatContainer
-        userChat={'아아 한 잔 줘'}
-        gptChat={`무엇을 도와드릴까요?\n다양한 커피 메뉴를 소개해드릴게요.\n커피 메뉴를 선택해주세요.\n오늘의 메뉴로 청포도 아이스 티를 추천해드려요!`}
+        userChat={userChat}
+        gptChat={gptChat}
         isRecording={isRecording}
         volume={volume}
         isExpand={location.pathname === '/kiosk'}
--- FE/kiosk/src/pages/OrderLayout/index.tsx
+++ FE/kiosk/src/pages/OrderLayout/index.tsx
@@ -2,12 +2,21 @@ import { Link, Outlet, useLocation } from 'react-router-dom';
 import Main from '@/pages/Main';
 import clsx from 'clsx';
 import { motion } from 'framer-motion';
+import RecordButton from '@/components/Chat/RecordButton';
+import { useState } from 'react';
 
 const OrderLayout = () => {
   const { pathname } = useLocation();
+  const [userChat, setUserChat] = useState<string>('');
+
+  const handleRecognitionResult = (text: string) => {
+    setUserChat(text);
+  };
+
   return (
     // 배경 색은 추후 변경 예정
     <div className='flex flex-col h-dvh bg-gradient-to-b from-primary to-white justify-between'>
+      {/* 임시 네비게이터 */}
       <div className='flex justify-center items-center z-10 fixed top-20 left-0 w-full h-1/3'>
         <ul className='flex justify-center items-center gap-4 bg-white rounded-xl p-4'>
           <li>
@@ -35,11 +44,15 @@ const OrderLayout = () => {
       </div>
       <header
         className={clsx(
-          'flex justify-center items-center',
+          'flex justify-center items-center relative',
           pathname === '/kiosk' ? 'h-full' : 'h-1/3'
         )}
       >
-        <Main userChat={''} gptChat={''} isRecording={false} volume={0} />
+        <Main
+          userChat={userChat}
+          gptChat={`무엇을 도와드릴까요?\n다양한 커피 메뉴를 소개해드릴게요.\n커피 메뉴를 선택해주세요.\n오늘의 메뉴로 청포도 아이스 티를 추천해드려요!`}
+        />
+        <RecordButton onRecognitionResult={handleRecognitionResult} />
       </header>
       <motion.main
         className='rounded-t-xl bg-white shadow-t-2xl flex flex-col justify-center items-center'
