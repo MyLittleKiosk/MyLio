d936b7235860dda0a0b847578276420af2c2b41b
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-05-08 09:52:10 +0900

    Feat: 로그인 시, 전역으로 유저 상태 저장 로직 추가

--- FE/admin/src/service/queries/user.ts
+++ FE/admin/src/service/queries/user.ts
@@ -1,13 +1,19 @@
 import { useMutation } from '@tanstack/react-query';
 import { login } from '@/service/apis/user';
 import { useNavigate } from 'react-router-dom';
+import { useUserStore } from '@/stores/useUserStore';
+import { User } from '@/types/user';
+import { Response } from '@/types/apiResponse';
 
 export function useLogin() {
   const navigate = useNavigate();
+  const { setUser } = useUserStore();
+
   return useMutation({
     mutationFn: ({ email, password }: { email: string; password: string }) =>
       login(email, password),
-    onSuccess: () => {
+    onSuccess: (data: Response<User>) => {
+      setUser(data.data);
       navigate('/');
     },
     onError: (error) => {
