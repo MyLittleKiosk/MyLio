d5f42d2e82f9020d88c5f0460200d54a80ffe52d
전아현 <dkgus4012@naver.com>
2025-05-09 09:26:31 +0900

    refactor: 전달 data 변경
    S12P31B102-189

--- BE/src/main/java/com/ssafy/mylio/domain/account/dto/response/AccountGetInfoResponseDto.java
+++ BE/src/main/java/com/ssafy/mylio/domain/account/dto/response/AccountGetInfoResponseDto.java
@@ -11,13 +11,13 @@ public class AccountGetInfoResponseDto {
     @Schema(example = "SUPER", description = "로그인한 계정의 역할입니다.")
     private String role;
 
-    @Schema(example = "qwer1@ssafy.io", description = "로그인한 계정의 아이디입니다.")
-    private String userID;
+    @Schema(example = "1", description = "로그인한 계정의 아이디입니다.")
+    private Integer userID;
 
     public static AccountGetInfoResponseDto of(Account account,String userRole){
         return AccountGetInfoResponseDto.builder()
                 .role(userRole)
-                .userID(account.getEmail())
+                .userID(account.getId())
                 .build();
     }
 
