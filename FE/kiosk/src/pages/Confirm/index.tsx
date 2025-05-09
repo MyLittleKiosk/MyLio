849ba33ef2f5192a35957c4c2d40739091270cec
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-05-09 16:04:47 +0900

    Feat: 페이지별 content 값 변경

--- FE/kiosk/src/pages/Confirm/index.tsx
+++ FE/kiosk/src/pages/Confirm/index.tsx
@@ -1,22 +1,21 @@
-import { CONFIRM_RESPONSE } from '@/service/mock/dummies/Order';
 import { formatNumber } from '@/utils/formatNumber';
-
+import useOrderStore from '@/stores/useOrderStore';
 const Confirm = () => {
+  const { order } = useOrderStore();
   return (
     <section className='flex flex-col w-full h-full ps-10 pt-10'>
       <h1 className='text-2xl font-preBold'>주문확인</h1>
       <div className='flex flex-col gap-2 mt-10 w-full overflow-y-auto'>
-        {CONFIRM_RESPONSE.confirm.map((item) => (
-          <div key={item.id} className='flex items-center gap-2'>
-            <img src={item.image} alt={item.name} />
+        {order.contents.map((item) => (
+          <div key={item.menuId} className='flex items-center gap-2'>
+            <img src={item.imageUrl} alt={item.name} />
             <div className='flex flex-col w-1/2'>
               <h2 className='text-lg font-preBold'>{item.name}</h2>
-              <p>{formatNumber(item.price)}</p>
+              <p>{formatNumber(item.basePrice)}</p>
               {item.options.map((option) => (
-                <p key={option.name} className='text-xs text-gray-500'>
+                <p key={option.optionName} className='text-xs text-gray-500'>
                   <div className='flex items-center justify-between'>
-                    <div>{option.name}</div>{' '}
-                    <div>+{formatNumber(option.price)}</div>
+                    <div>{option.optionName}</div>{' '}
                   </div>
                 </p>
               ))}
