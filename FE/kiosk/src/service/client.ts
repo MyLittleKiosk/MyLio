c3e6840ad65bfe57dc456b72a24a872ac4f21c6e
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-05-09 13:09:40 +0900

    Feat: api 연동을 위한 authClient 구성

--- FE/kiosk/src/service/client.ts
+++ FE/kiosk/src/service/client.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseUrl = import.meta.env.VITE_PUBLIC_API_URL;
+
 const client = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: baseUrl,
 });
 
 export default client;
