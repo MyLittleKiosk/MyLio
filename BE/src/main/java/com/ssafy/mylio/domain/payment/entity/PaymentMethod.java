7e34e641177d69e1e7561d8bcfb24d925d722191
singwisdom <57509973+singwisdom@users.noreply.github.com>
2025-05-11 18:13:57 +0900

    Refactor: PaymentMethod ENUM 업데이트
    S12P31B102-198

--- BE/src/main/java/com/ssafy/mylio/domain/payment/entity/PaymentMethod.java
+++ BE/src/main/java/com/ssafy/mylio/domain/payment/entity/PaymentMethod.java
@@ -2,11 +2,13 @@ package com.ssafy.mylio.domain.payment.entity;
 
 import com.ssafy.mylio.global.common.status.EntityStatus;
 
+import java.util.Arrays;
+
 public enum PaymentMethod implements EntityStatus {
     CARD("CARD", "카드"),
-    CASH("CASH", "현금"),
+    CASH("PAY", "카카오페이"),
     MOBILE("MOBILE", "모바일결제"),
-    POINTS("POINTS", "포인트");
+    POINTS("GIFT", "기프트 카드");
 
     private final String code;
     private final String description;
@@ -25,4 +27,12 @@ public enum PaymentMethod implements EntityStatus {
     public String getDescription() {
         return description;
     }
+
+    public static PaymentMethod fromCode(String code) {
+        return Arrays.stream(PaymentMethod.values())
+                .filter(p -> p.getCode().equalsIgnoreCase(code))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Unknown payment method code: " + code));
+    }
+
 }
