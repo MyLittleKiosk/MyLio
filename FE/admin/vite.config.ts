d649a5fed1c8a5d119ea21a68cc5e59b76aed34d
이하영 <lhy23456@gmail.com>
2025-05-12 13:57:39 +0900

    Chore: vite config 돌리는 커밋

--- FE/admin/vite.config.ts
+++ FE/admin/vite.config.ts
@@ -1,16 +1,9 @@
 import react from '@vitejs/plugin-react';
-// import fs from 'fs';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.BASE_PATH || '/',
   plugins: [react(), tsconfigPaths()],
-  // server: {
-  //   https: {
-  //     key: fs.readFileSync('./localhost-key.pem'),
-  //     cert: fs.readFileSync('./localhost.pem'),
-  //   },
-  //   port: 3000,
-  // },
 });
