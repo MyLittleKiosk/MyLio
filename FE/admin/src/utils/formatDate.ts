2fbaf54ced6d3ce83ad8326e60c883b8d5c146d1
강성엽 <piz115@naver.com>
2025-05-13 13:49:02 +0900

    Chore: 불필요한 리턴 수정

--- FE/admin/src/utils/formatDate.ts
+++ FE/admin/src/utils/formatDate.ts
@@ -3,17 +3,14 @@
  * @param str 날짜 문자열
  * @returns 변환된 날짜 문자열
  */
-export const formatDate = (str: string) => {
-  return (() => {
-    const date = new Date(str);
-    return date.toLocaleString('ko-KR', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: false,
-      timeZone: 'Asia/Seoul',
-    });
-  })();
+export const formatDate = (date: string) => {
+  return new Date(date).toLocaleString('ko-KR', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+    timeZone: 'Asia/Seoul',
+  });
 };
