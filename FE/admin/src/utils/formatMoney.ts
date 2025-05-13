135f625a11b2d8d19ffac36ef4c2c6f6b2d94b48
강성엽 <piz115@naver.com>
2025-05-13 12:58:15 +0900

    Comment: 원화 변환 유틸 주석 추가

--- FE/admin/src/utils/formatMoney.ts
+++ FE/admin/src/utils/formatMoney.ts
@@ -1,3 +1,8 @@
+/**
+ * 숫자를 원화 형식으로 변환합니다.
+ * @param money 숫자
+ * @returns 원화 형식의 문자열
+ */
 const formatMoney = (money: number) => {
   return money.toLocaleString('ko-KR', {
     style: 'currency',
