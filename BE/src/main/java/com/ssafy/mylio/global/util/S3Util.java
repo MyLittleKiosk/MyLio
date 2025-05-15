98b03b519b7f18a02e1086b932ec1312812a6011
이병조 <qudwh0312@naver.com>
2025-05-15 16:53:05 +0900

    Refactor : AWS S3 의존서 교체

--- BE/src/main/java/com/ssafy/mylio/global/util/S3Util.java
+++ BE/src/main/java/com/ssafy/mylio/global/util/S3Util.java
@@ -1,25 +1,25 @@
 package com.ssafy.mylio.global.util;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.time.LocalDate;
 import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
 public class S3Util {
 
-    private final AmazonS3 amazonS3;
+    private final S3Client s3Client;
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
@@ -29,25 +29,33 @@ public class S3Util {
         // 키 생성 (중복 방지 및 디렉터리 관리)
         String key = generateKey(imageFile.getOriginalFilename());
 
-        // 메타데이터 설정
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentType(imageFile.getContentType());
-        metadata.setContentLength(imageFile.getSize());
+        PutObjectRequest putReq = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .contentType(imageFile.getContentType())
+                .contentLength(imageFile.getSize())
+                .acl(ObjectCannedACL.PRIVATE)
+                .build();
 
-        // 4) 업로드 요청 (InputStream 스트리밍)
+        // 3) 스트림과 함께 업로드
         try (InputStream is = imageFile.getInputStream()) {
-            PutObjectRequest request = new PutObjectRequest(bucket, key, is, metadata)
-                    .withCannedAcl(CannedAccessControlList.Private);  // 기본 비공개
-            amazonS3.putObject(request);
+            s3Client.putObject(putReq,
+                    RequestBody.fromInputStream(is, imageFile.getSize()));
         }
 
+        // 4) 업로드된 객체의 URL 생성
+        GetUrlRequest urlReq = GetUrlRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
 
-        return amazonS3.getUrl(bucket, key).toString();
+
+        return s3Client.utilities().getUrl(urlReq).toString();
     }
 
     private String generateKey(String filename){
         String ext = StringUtils.getFilenameExtension(filename);
-        return LocalDate.now() +"/" + UUID.randomUUID() + "." + ext;
+        return "menu" +"/" + UUID.randomUUID() + "." + ext;
     }
 
 }
