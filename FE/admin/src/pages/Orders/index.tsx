b0547c2b73af70ca5e0c2290ae5bf7c161fa73c0
이하영 <lhy23456@gmail.com>
2025-05-16 13:00:56 +0900

    Chore: conflict 해결

--- FE/admin/src/pages/Orders/index.tsx
+++ FE/admin/src/pages/Orders/index.tsx
@@ -11,58 +11,6 @@ const Orders = () => {
         <OrderContent />
       </Suspense>
     </ErrorBoundary>
-    <>
-      <section className='w-full h-full p-4 flex flex-col gap-2 '>
-        <h1 className='text-2xl font-preBold h-[5%]'>주문 관리</h1>
-        <div className='flex gap-2 max-h-[10%] w-full justify-between'>
-          <div className='flex gap-2 w-full'>
-            <Input
-              id='startSearchOrder'
-              placeholder=''
-              type='date'
-              value={startSearchValue}
-              maxDate={endSearchValue}
-              onChange={handleSearchChange}
-            />
-            <Input
-              id='endSearchOrder'
-              placeholder=''
-              type='date'
-              value={endSearchValue}
-              minDate={startSearchValue}
-              onChange={handleEndSearchChange}
-            />
-            <Button
-              id='searchBtnId'
-              type='button'
-              text='검색'
-              onClick={handleSearch}
-            />
-            <Button
-              id='resetBtnId'
-              type='button'
-              text='전체'
-              onClick={handleReset}
-              className='bg-content hover:bg-content2'
-            />
-          </div>
-        </div>
-        <Table
-          title='주문 목록'
-          description={`총 ${ordersData?.length || 0}개의 주문이 있습니다.`}
-          columns={ORDER_COLUMNS}
-          data={ordersData as OrderType[]}
-          onView={(row) => {
-            openModal(<ViewDetailOrderModal initialData={row} />);
-          }}
-        />
-        <PageNavigation
-          pageInfo={pageInfo as Pagination}
-          onChangePage={(page: number) => handlePageChange(page)}
-        />
-        <Modal />
-      </section>
-    </>
   );
 };
 
