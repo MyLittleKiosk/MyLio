a8a3b065810eb0435b424294658671138b197fd1
강성엽 <piz115@naver.com>
2025-05-19 09:33:03 +0900

    !HOTFIX: 이미지 경로 수정

--- FE/kiosk/src/datas/IMAGE_ROUTE.ts
+++ FE/kiosk/src/datas/IMAGE_ROUTE.ts
@@ -1,8 +1,14 @@
+import confirm from '@/assets/images/confirm.png';
+import detail from '@/assets/images/detail.png';
+import order from '@/assets/images/order.png';
+import search from '@/assets/images/search.png';
+import selectPay from '@/assets/images/select_pay.png';
+
 export const IMAGE_ROUTE = {
-  search: '/src/assets/images/search.png',
-  detail: '/src/assets/images/detail.png',
-  'select-pay': '/src/assets/images/select_pay.png',
-  order: '/src/assets/images/order.png',
-  confirm: '/src/assets/images/confirm.png',
-  pay: '/src/assets/images/select_pay.png',
+  search,
+  detail,
+  'select-pay': selectPay,
+  order,
+  confirm,
+  pay: selectPay,
 };
