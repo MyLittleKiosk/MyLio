be328a108ffa7dec1f429335219e9669cf8928d8
이하영 <lhy23456@gmail.com>
2025-05-19 14:44:03 +0900

    Fix: 메뉴 캐러셀 초기화로직 추가 및 현재 위치 확인 디자인 추가

--- FE/kiosk/src/pages/Menus/index.tsx
+++ FE/kiosk/src/pages/Menus/index.tsx
@@ -1,23 +1,38 @@
 import useOrderStore from '@/stores/useOrderStore';
-import { useMemo, useState } from 'react';
+import clsx from 'clsx';
+import { useEffect, useMemo, useState } from 'react';
 
 const Menus = () => {
   const { order } = useOrderStore();
   const [page, setPage] = useState(0);
+
   const menus = useMemo(() => {
-    if (order.contents.length === 0) return [];
+    if (order.contents.length === 0) {
+      setPage(0);
+      return [];
+    }
+
     const itemsPerPage = 9;
     const totalPages = Math.ceil(order.contents.length / itemsPerPage);
     const result = [];
-    console.log('order.contents:', order.contents);
+
     for (let i = 0; i < totalPages; i++) {
       const start = i * itemsPerPage;
       const end = start + itemsPerPage;
       result.push(order.contents.slice(start, end));
     }
-    console.log('result:', result);
+
+    if (totalPages === 1) {
+      setPage(0);
+    }
+
     return result;
   }, [order.contents]);
+
+  useEffect(() => {
+    setPage(0);
+  }, [order.contents]);
+
   return (
     <section className='flex flex-col w-full h-full pt-5'>
       <h1 className='text-2xl font-preBold inline-block ps-10'>메뉴</h1>
@@ -26,9 +41,13 @@ const Menus = () => {
           총 {order.contents.length}개의 메뉴
         </span>
       </div>
-      <div className='flex justify-between w-[95%] mx-auto  h-[65%]'>
+      <div className='flex justify-between w-[95%] mx-auto h-[65%]'>
         <div className='flex h-full items-center justify-center gap-2'>
-          <button onClick={() => setPage(page - 1)} disabled={page === 0}>
+          <button
+            onClick={() => setPage(page - 1)}
+            disabled={page === 0}
+            className={clsx('block', page === 0 && 'hidden')}
+          >
             {'<'}
           </button>
         </div>
@@ -57,9 +76,20 @@ const Menus = () => {
         <button
           onClick={() => setPage(page + 1)}
           disabled={page === menus.length - 1}
+          className={clsx('block', page === menus.length - 1 && 'hidden')}
         >
           {'>'}
         </button>
+      </div>
+      <div className='flex justify-center items-center gap-2 mt-1'>
+        {menus.map((_, index) => (
+          <div
+            key={index}
+            className={`w-2 h-2 rounded-full transition-all duration-200 ${
+              page === index ? 'bg-black scale-125' : 'bg-subContent'
+            }`}
+          />
+        ))}
       </div>
     </section>
   );
