17eab29a0c0a8969aae480162f5e7f904479158b
이하영 <lhy23456@gmail.com>
2025-05-19 16:27:44 +0900

    Fix: 수량변경 프롬프트 텍스트 수정

--- FE/kiosk/src/pages/Confirm/Item.tsx
+++ FE/kiosk/src/pages/Confirm/Item.tsx
@@ -1,4 +1,8 @@
+import { useDebounce } from '@/hooks/useDebounce';
+import { useOrderRequest } from '@/service/queries/order';
+import useOrderStore from '@/stores/useOrderStore';
 import clsx from 'clsx';
+import { useState } from 'react';
 
 interface OptionDetail {
   optionDetailValue: string;
@@ -11,26 +15,83 @@ interface Option {
 }
 
 interface ItemProps {
+  menuId: number;
   imageUrl: string;
   name: string;
   selectedOption: Option[];
   totalPrice: number;
   count: number;
   isLast?: boolean;
-  onIncrease: () => void;
-  onDecrease: () => void;
 }
 
 const Item = ({
+  menuId,
   imageUrl,
   name,
   selectedOption,
   totalPrice,
   count,
   isLast,
-  onIncrease,
-  onDecrease,
 }: ItemProps) => {
+  const { mutate } = useOrderRequest();
+  const { order, setOrder } = useOrderStore();
+  const [counter, setCounter] = useState(0);
+
+  const debouncedMutate = useDebounce(
+    (params: Parameters<typeof mutate>[0]) => {
+      mutate(params, {
+        onSuccess: () => {
+          setCounter(0);
+        },
+      });
+    },
+    800
+  );
+
+  const increaseCount = (menuId: number) => {
+    const cartItem = order.cart.find((item) => item.menuId === menuId);
+    const quantity = cartItem ? cartItem.quantity + 1 : 1;
+
+    setOrder({
+      ...order,
+      cart: order.cart.map((item) =>
+        item.menuId === menuId ? { ...item, quantity } : item
+      ),
+      contents: order.contents.map((item) =>
+        item.menuId === menuId ? { ...item, quantity } : item
+      ),
+    });
+
+    setCounter(counter + 1);
+
+    debouncedMutate({
+      text: `cartId가 ${cartItem?.cartId}이고, cartName이 ${cartItem?.name}인 메뉴의 수량을 ${counter + 1}만큼 추가해 주세요.`,
+      ...order,
+    });
+  };
+
+  const decreaseCount = (menuId: number) => {
+    const cartItem = order.cart.find((item) => item.menuId === menuId);
+    const quantity = cartItem ? cartItem.quantity - 1 : 0;
+
+    setOrder({
+      ...order,
+      cart: order.cart.map((item) =>
+        item.menuId === menuId ? { ...item, quantity } : item
+      ),
+      contents: order.contents.map((item) =>
+        item.menuId === menuId ? { ...item, quantity } : item
+      ),
+    });
+
+    setCounter(counter - 1);
+
+    debouncedMutate({
+      text: `cartId가 ${cartItem?.cartId}이고, cartName이 ${cartItem?.name}인 메뉴의 수량을 ${counter - 1}만큼 감소해 주세요.`,
+      ...order,
+    });
+  };
+
   return (
     <div
       className={clsx(
@@ -65,7 +126,7 @@ const Item = ({
         <div className='flex items-center bg-gray-100 rounded-xl px-3 py-1 gap-2 w-fit'>
           <button
             className='text-xl font-bold text-gray-400'
-            onClick={onDecrease}
+            onClick={() => decreaseCount(menuId)}
             disabled={count <= 1}
           >
             -
@@ -75,7 +136,7 @@ const Item = ({
           </span>
           <button
             className='text-xl font-bold text-gray-400'
-            onClick={onIncrease}
+            onClick={() => increaseCount(menuId)}
           >
             +
           </button>
--- FE/kiosk/src/pages/Confirm/index.tsx
+++ FE/kiosk/src/pages/Confirm/index.tsx
@@ -1,59 +1,9 @@
 import { formatNumber } from '@/utils/formatNumber';
 import useOrderStore from '@/stores/useOrderStore';
 import Item from '@/pages/Confirm/Item';
-import { useOrderRequest } from '@/service/queries/order';
-import { useDebounce } from '@/hooks/useDebounce';
 
 const Confirm = () => {
-  const { order, setOrder } = useOrderStore();
-  const { mutate } = useOrderRequest();
-
-  const debouncedMutate = useDebounce(
-    (params: Parameters<typeof mutate>[0]) => {
-      mutate(params);
-    },
-    800
-  );
-
-  const increaseCount = (menuId: number) => {
-    const cartItem = order.cart.find((item) => item.menuId === menuId);
-    const quantity = cartItem ? cartItem.quantity + 1 : 1;
-
-    setOrder({
-      ...order,
-      cart: order.cart.map((item) =>
-        item.menuId === menuId ? { ...item, quantity } : item
-      ),
-      contents: order.contents.map((item) =>
-        item.menuId === menuId ? { ...item, quantity } : item
-      ),
-    });
-
-    debouncedMutate({
-      text: `${cartItem?.cartId}의 ${cartItem?.name}를 ${quantity}로 바꿔주세요`,
-      ...order,
-    });
-  };
-
-  const decreaseCount = (menuId: number) => {
-    const cartItem = order.cart.find((item) => item.menuId === menuId);
-    const quantity = cartItem ? cartItem.quantity - 1 : 0;
-
-    setOrder({
-      ...order,
-      cart: order.cart.map((item) =>
-        item.menuId === menuId ? { ...item, quantity } : item
-      ),
-      contents: order.contents.map((item) =>
-        item.menuId === menuId ? { ...item, quantity } : item
-      ),
-    });
-
-    debouncedMutate({
-      text: `${cartItem?.cartId}의 ${cartItem?.name}를 ${quantity}로 바꿔주세요`,
-      ...order,
-    });
-  };
+  const { order } = useOrderStore();
 
   return (
     <section className='flex flex-col w-full h-full pt-5'>
@@ -64,14 +14,13 @@ const Confirm = () => {
         {order.contents.map((item, idx) => (
           <Item
             key={item.menuId}
+            menuId={item.menuId}
             imageUrl={item.imageUrl}
             name={item.name}
             selectedOption={item.selectedOption}
             totalPrice={item.totalPrice}
             count={item.quantity}
             isLast={idx === order.cart.length - 1}
-            onIncrease={() => increaseCount(item.menuId)}
-            onDecrease={() => decreaseCount(item.menuId)}
           />
         ))}
         <div className='flex flex-col gap-2 w-full pb-6'>
